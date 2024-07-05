ea0e2fd9ab8c6280f7ad8686eaead27499101dce
SerimPark <s_hi32@naver.com>
2024-07-05 13:56:04 +0900

    initial commit

---
+++ src/main/kotlin/com/example/kopringplayground/KopringPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringPlaygroundApplication>(*args)
+}
