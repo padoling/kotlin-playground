00c6f97f8d016d530a4666d52d28896f98a6d9df
SerimPark <s_hi32@naver.com>
2024-07-05 16:52:10 +0900

    add extensions of localdatetime and string

---
+++ src/test/kotlin/com/example/kopringplayground/study/ExtensionTest.kt
@@ -0,0 +1,25 @@
+package com.example.kopringplayground.study
+
+import com.example.kopringplayground.extension.format
+import com.example.kopringplayground.extension.toSlug
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class ExtensionTest {
+    @Test
+    fun `String tuSlug test`() {
+        val str = "Hello, World"
+        val expected = "hello-world"
+        val actual = str.toSlug()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `LocalDateTime format test`() {
+        val date = LocalDateTime.of(2021, 9, 1, 0, 0, 0)
+        val expected = "2021-09-01 1st 2021"
+        val actual = date.format()
+        assertThat(actual).isEqualTo(expected)
+    }
+}
